45eadee2a6703a5960e23a9ef5a3f67b6f7482c6
SHIVENDRAIYER <shivendrav13@gmail.com>
2021-04-20 09:31:22 +0530

    DisplayHotelDetails Added

--- tms/src/App.js
+++ tms/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import DisplayHotelDetails from './component/Hotel/DisplayHotelDetails';
 
 function App() {
+
+  let mockHotel = {
+    hotelId: 5,
+    hotelName: "A2B",
+    hotelType: "Super",
+    hotelDescription: "Five Stars",
+    address: "chennai",
+    rent: 1000.0
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <DisplayHotelDetails hotel={mockHotel} />
     </div>
+
   );
 }
 
