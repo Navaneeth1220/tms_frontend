05ce666507f7b493df91771af5e2480aa123814c
SHIVENDRAIYER <shivendrav13@gmail.com>
2021-04-20 10:28:42 +0530

    GetHotelDetails Added

--- tms/src/App.js
+++ tms/src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import DisplayHotelDetails from './component/Hotel/DisplayHotelDetails';
+import GetHotelDetails from './component/Hotel/GetHotelDetails';
 
 function App() {
 
-  let mockHotel = {
-    hotelId: 5,
-    hotelName: "A2B",
-    hotelType: "Super",
-    hotelDescription: "Five Stars",
-    address: "chennai",
-    rent: 1000.0
-  }
+  /* let mockHotel = {
+     hotelId: 5,
+     hotelName: "A2B",
+     hotelType: "Super",
+     hotelDescription: "Five Stars",
+     address: "chennai",
+     rent: 1000.0
+   }*/
 
   return (
 
     <div>
-      <DisplayHotelDetails hotel={mockHotel} />
+
+      {/* <DisplayHotelDetails hotel={mockHotel} />*/}
+      {<GetHotelDetails />}
+
+      
     </div>
 
   );
