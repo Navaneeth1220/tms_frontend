b26c87ecbbb2a553ba826464986f9d24e1696d4c
ajysam <ajaysamuel.samuel@gmail.com>
2021-04-24 10:10:05 +0530

    changes made

---
+++ tms/src/service/FeedbackService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:8181";
+
+function fetchFeedbackById(feedbackId) {
+
+    const url = baseUrl + "/feedbacks/byid/" + feedbackId;
+    const promise = axios.get(url);
+    return promise;
+
+}
+
+function addFeedback(data) {
+
+    const url = baseUrl + "/feedbacks/add";
+    let requestData = {
+
+        feedback: data.feedback,
+        customerId: data.customerId,
+        rating: data.rating
+    };
+
+    console.log("inside addPackage service, request data", requestData);
+    const promise = axios.post(url, requestData);
+    return promise;
+
+}
+
+function fetchAllFeedbacks() {
+
+    const url = baseUrl + "/feedbacks";
+    const promise = axios.get(url);
+    return promise;
+
+}
+
+export { fetchFeedbackById, addFeedback, fetchAllFeedbacks };
