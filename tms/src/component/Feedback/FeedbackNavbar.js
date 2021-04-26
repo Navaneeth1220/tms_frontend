489df2ddc3c648ff2d521d67ec763973b050ed66
ajysam <ajaysamuel.samuel@gmail.com>
2021-04-27 00:48:42 +0530

    new changes added

---
+++ tms/src/component/Feedback/FeedbackNavbar.js
@@ -0,0 +1,16 @@
+
+import { Link } from "react-router-dom";
+
+function OrderNavbar() {
+  return (
+    <div className="container navbar mt-3">
+      <Link to="/feedbackhome/feedbackdetails/:id">Feedback Details by Feedback id</Link>
+
+      <Link to="/feedbackhome/feedbacklist/:id"> List of Feedbacks by Feedback id</Link>
+
+      <Link to="/feedbackhome/feedbackbycustomerid/:id">Feedback Details by Customer id</Link>
+    </div>
+  );
+}
+
+export default FeedbackNavbar;
