df8cf1edd43caf4dac7d7a1c4af18fa8fbf0b66b
Navaneeth S <nava8092@gmail.com>
2021-04-29 16:42:02 +0530

    before merge

--- tms/src/redux/bookingredux/store.js
+++ tms/src/redux/bookingredux/store.js
@@ -1,9 +1,9 @@
 import { applyMiddleware , combineReducers , createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import addBookingReducer from "./bookingredux/addBooking/addBookingReducer"
-import getBookingOnRequestReducer from "./bookingredux/getbookingonrequest/getBookingOnRequestReducer";
-import getBookingOnUserIdRequestReducer from "./bookingredux/getBookingOnUserIdRequest/getBookingOnUserIdRequestReducer";
+import addBookingReducer from "./addBooking/addBookingReducer"
+import getBookingOnRequestReducer from "./getbookingonrequest/getBookingOnRequestReducer";
+import getBookingOnUserIdRequestReducer from "./getBookingOnUserIdRequest/getBookingOnUserIdRequestReducer";
 
 const store = createStore (
 
