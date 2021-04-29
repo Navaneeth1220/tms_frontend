f0af1ff8bb2650ad5b05ca1851713fc401faaaff
Gunaditha <gunadithakushi@gmail.com>
2021-04-29 13:45:09 +0530

    report home completed

--- tms/src/App.js
+++ tms/src/App.js
@@ -1,20 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import store from "./redux/store";
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import AddReport from './component/reportcomponent/AddReport';
-import GetReportOnDetailsOnRequest from './component/reportcomponent/GetReportDetailsOnRequest';
+
 
 
 
 function App() {
   return (
     <div>
-      <Provider store={store} >
-        {/*<AddReport />*/}
-        {<GetReportOnDetailsOnRequest />}
-      </Provider>
+     
     </div>
   );
 }
