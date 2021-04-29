709c3684474ad2c3c512c8d491f46b0b17dc0e71
SHIVENDRAIYER <shivendrav13@gmail.com>
2021-04-29 14:39:15 +0530

    HotelHome and PackageHome Updated

--- tms/src/component/hotelcomponent/HotelHome.js
+++ tms/src/component/hotelcomponent/HotelHome.js
@@ -1,18 +1,43 @@
 
 import commonStyle from "./commonStyle.module.css";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 
 export default function HotelHome() {
 
   return (
 
-    <div>
-      <h1> Hotel App </h1>
+    <div className="container mt-5">
+      <h1> Hotel Module </h1>
 
       <div className={commonStyle.content}>
         <div>
-          This is our Hotel app
-          <br />
-         please use the available features
+          <br /> The features available are :
+        <ul className="list-group mt-4">
+
+            <li className="nav-item">
+
+              <Link to="/addhotel" className="nav-link" >
+                <span>Add Hotel</span>
+
+              </Link>
+            </li>
+
+            <li className="nav-item">
+
+              <Link to="/allhotels" className="nav-link" >
+                <span>Get All Hotels</span>
+              </Link>
+            </li>*
+
+            <li className="nav-item">
+
+              <Link to="/hoteldetailsonrequest" className="nav-link" >
+                <span>Get Hotel Details </span>
+              </Link>
+            </li>
+
+          </ul>
         </div>
       </div>
     </div>
--- tms/src/component/packagecomponent/PackageHome.js
+++ tms/src/component/packagecomponent/PackageHome.js
@@ -5,14 +5,49 @@ export default function PackageHome() {
 
   return (
 
-    <div>
-      <h1> Package App </h1>
+
+    <div className="container mt-5">
+      <h1> Package Module </h1>
 
       <div className={commonStyle.content}>
         <div>
-          This is Package app
-          <br />
-          please use the available features
+          <br /> The features available are :
+        <ul className="list-group mt-4">
+
+            <li className="nav-item">
+
+              <Link to="/addpackage" className="nav-link" >
+                <span>Add Package</span>
+
+              </Link>
+            </li>
+
+            <li className="nav-item">
+
+              <Link to="/updatepackage" className="nav-link" >
+                <span>Update Package</span>
+
+              </Link>
+            </li>
+
+
+
+
+            <li className="nav-item">
+
+              <Link to="/allpackages" className="nav-link" >
+                <span>Get All Packages</span>
+              </Link>
+            </li>*
+
+            <li className="nav-item">
+
+              <Link to="/packagedetailsonrequest" className="nav-link" >
+                <span>Get Package Details</span>
+              </Link>
+            </li>
+
+          </ul>
         </div>
       </div>
     </div>
