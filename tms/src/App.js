1b898f033e1e3abe9a1ca4e007714ae033e39fa9
MohanaSundaramPurushothaman <mp1429@srmist.edu.in>
2021-04-29 13:39:55 +0530

    Customer Home Done

--- tms/src/App.js
+++ tms/src/App.js
@@ -1,31 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import AddCustomer from "./components/customer/AddCustomer";
-//import Home from "./components/Customer/Home";
-//import Navbar from "./components/Customer/NavBar";
-import store from "./redux/store";
-import { Provider } from 'react-redux';
-//import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-//import GetCustomerDetailsById from "./components/Customer/GetCustomerDetailsById";
-//import GetCustomerDetailsByRouteId from"./components/Customer/GetCustomerDetailsRouteId";
-//import GetCustomerDetailsPackageId from"./components/Customer/GetCustomerDetailsPackageId";
-import UpdateCustomer from './components/customer/UpdateCustomer';
-
 function App() {
 
   
   return (
 
     <div>
-     {/*<GetCustomerDetailsPackageId/>*/}
-     {/*<GetCustomerDetailsByRouteId/>*/}
-   
-    <Provider store={store} >
-      {/*<AddCustomer/>*/}
-
-    {<UpdateCustomer/>}
-      
-  </Provider>
+     
   </div>
 
     
